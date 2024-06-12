6161e59e6f98866d78af4b8edb394c1ff9cd00cc
Navyanth03 <mekalanavyanthyadav2003@gmail.com>
2024-06-12 14:24:38 +0530

    modified styles

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -38,28 +38,32 @@ export default function Meme() {
     return (
         <main>
             <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                    name="topText"
-                    value={meme.topText}
-                    onChange={handleChange}
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
-                    Get a new meme image 🖼
-                </button>
+                <div className="addtext">
+                    <input 
+                        type="text"
+                        placeholder="Top text"
+                        className="form--input"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Bottom text"
+                        className="form--input"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <button 
+                        className="form--button"
+                        onClick={getMemeImage}
+                    >
+                        New meme 
+                    </button>
+                </div>
             </div>
             <div className="meme">
                 <img src={meme.randomImage} className="meme--image" />
